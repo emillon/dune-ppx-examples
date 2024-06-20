cdd96705c47210f4205dc660c0c6ac6e9ab48fca
Etienne Millon <me@emillon.org>
2024-06-20 09:55:41 +0200

    error reporting

--- 01_defining_a_deriver/ppx/deriver.ml
+++ 01_defining_a_deriver/ppx/deriver.ml
@@ -1,17 +1,18 @@
 open Ppxlib
 
 let generate_impl_for_one (decl : Parsetree.type_declaration) =
+  let open Ast_builder.Make (struct
+    let loc = decl.ptype_loc
+  end) in
   match decl.ptype_kind with
   | Ptype_variant cds ->
-      let open Ast_builder.Make (struct
-        let loc = decl.ptype_loc
-      end) in
       let exprs = List.map (fun cd -> econstruct cd None) cds in
       let every_name = Printf.sprintf "every_%s" decl.ptype_name.txt in
       let pat = ppat_var (Loc.make ~loc every_name) in
       let expr = elist exprs in
       pstr_value_list ~loc Nonrecursive [ value_binding ~pat ~expr ]
-  | _ -> (* TODO error reporting *) assert false
+  | _ ->
+      [ pstr_extension (Location.error_extensionf ~loc "Unsupported type") [] ]
 
 let generate_impl ~ctxt:_ (_rec, decls) =
   List.concat_map generate_impl_for_one decls
